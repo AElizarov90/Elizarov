840a6c4491a0e2819e90b5283672336ad1422794
AElizarov90 <mekaomusic@gmail.com>
2017-10-18 20:32:51 +0600

    Лабораторная 2 Задание1 Задание2

---
+++ лабораторная2/zadanie2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String a;
+            a = Console.ReadLine();
+            int A;
+            A = int.Parse(a);
+            int result;
+            result = 6 * A * A;
+            Console.Write(result);
+
+
+        }
+    }
+}
