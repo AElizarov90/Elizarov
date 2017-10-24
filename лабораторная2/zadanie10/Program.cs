ffd3b4a556f021fc08c89dd59aba4b935f46543c
AElizarov90 <mekaomusic@gmail.com>
2017-10-24 20:42:51 +0600

    лаба 2,до 10

---
+++ лабораторная2/zadanie10/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double xInGrad;
+            xInGrad = Convert.ToDouble(Console.ReadLine());
+            double result;
+            result = Math.Sin(xInGrad);
+            Console.WriteLine("{0:F4}", result);
+        }
+    }
+}
--- лабораторная2/zadaniee6/Program.cs
+++ лабораторная2/zadaniee6/Program.cs
@@ -16,22 +16,27 @@ namespace zadaniee6
             height = Convert.ToDouble(Console.ReadLine());
             double area;
             if (base1 > 0)
-                            
-                {
-
-
-                    area = base1 * height / 2;
-                    Console.WriteLine("{0:F2}", area);
-                }
-                else Console.WriteLine("Значение base1 должно быть положительным");
-            if (height > 0)
+            if (height > 0) 
+        
             {
                 area = base1 * height / 2;
-                Console.WriteLine("{1:F2}", area);
+                Console.WriteLine("{0:F2}", area);
+            }
+            else
+            {
+
+                Console.WriteLine("Значение base1 должно быть положительным");
+            }
+                                 
+            else
+            {
+
+                Console.WriteLine("Значение height должно быть положительным");
+
             }
 
 
-            else Console.WriteLine("Значение height должно быть положительным");
+
             
 
 
