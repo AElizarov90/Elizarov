00a1f4ff1ca0bfef8c06f13a37ea99ef263901ee
AElizarov90 <mekaomusic@gmail.com>
2017-11-07 19:47:01 +0600

    1-4

---
+++ lab3/zadanie5/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double distanceInKm;
+            distanceInKm = Convert.ToDouble(Console.ReadLine());
+            if (distanceInKm < 0)
+            {
+                Console.WriteLine("Значение distanceInKm должно быть неотрицательным");
+                    return;
+                    
+            }
+            double distanceInFeet;
+            distanceInFeet = Convert.ToDouble(Console.ReadLine());
+            if (distanceInFeet < 0)
+            {
+                Console.WriteLine("Значение distanceInFeet должно быть неотрицательным");
+                return;
+            }
+             double R;
+            R = distanceInKm * 0.305* 0.305* 0.305;
+            if (R < distanceInFeet)
+            {
+                Console.WriteLine("Расстояние в километрах меньше");
+
+            }
+            else if (R > distanceInFeet)
+            {
+                Console.WriteLine("Расстояние в футах меньше");
+            
+            
+
+            }
+            
+
+            
+
+        }
+    }
+}
---
+++ lab3/zadanie6/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double velocityInKmH;
+            velocityInKmH = Convert.ToDouble(Console.ReadLine());
+            if (velocityInKmH < 0)
+            {
+                Console.WriteLine("Значение velocityInKmH должно быть неотрицательным");
+                return;
+
+            }
+            double velocityInMS;
+            velocityInMS = Convert.ToDouble(Console.ReadLine());
+            if (velocityInMS < 0)
+            {
+                Console.WriteLine("Значение velocityInMS должно быть неотрицательным");
+                return;
+
+            }
+            double R;
+            R = velocityInMS * 60 * 100;
+            double R2;
+            R2 = velocityInKmH;
+            if (R < R2)
+            {
+                Console.WriteLine("{0:F2} км/ч меньше {1:F2} м/с", velocityInKmH, velocityInMS);
+            }
+            else 
+            {
+                Console.WriteLine("{0:F2} м/с меньше {1:F2} км/ч", velocityInMS, velocityInKmH);
+
+            }
+        }
+    }
+}
