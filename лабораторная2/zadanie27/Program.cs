fbcc051e873d6929ce231270cc2dbf2c825247f7
AElizarov90 <mekaomusic@gmail.com>
2017-12-03 14:04:24 +0300

    laba 2 full,laba 5 full,laba 6 - 1-11

--- лабораторная2/zadanie27/Program.cs
+++ лабораторная2/zadanie27/Program.cs
@@ -10,6 +10,11 @@ namespace zadanie27
     {
         static void Main(string[] args)
         {
+            double X;
+            X = Convert.ToDouble(Console.ReadLine());
+            double result;
+            result = Math.Sqrt(1 - (Math.Pow(Sin,2(X);
+            Console.WriteLine("{0:F4}", result);
         }
     }
 }
