6a5de562147c595c1fcc38413a729a4ba30642d0
AElizarov90 <mekaomusic@gmail.com>
2018-01-04 14:12:52 +0600

    lab 6 zadanie 1-34

---
+++ Lab6/zadanie1/zadanie8/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int f= 453;
+            int a;
+            a = Convert.ToInt16(Console.ReadLine());
+
+        }
+    }
+}
---
+++ Lab6/zadanie21/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int a = 10, b = 20;
+            while (a <= b)
+            {
+                if (x == a)
+                {
+                    Console.WriteLine(x + "+");
+                    
+                }
+                else
+                {
+                    Console.WriteLine(a);
+                   
+                }
+                a = a + 1;
+            }
+        }
+    }
+}
+
+
